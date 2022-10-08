29651d76734c4bb9690c1436f756bd9a229b5dcb
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2022-10-08 11:00:56 -0400

    feat: implement cursor

---
+++ sceneControl.py
@@ -0,0 +1,11 @@
+import pygame
+
+def cursorDraw(surface):
+    posX, posY = pygame.mouse.get_pos()
+
+    pygame.draw.circle(surface, [255, 255, 255], (posX, posY), 5, width = 1)
+    pygame.draw.circle(surface, [255, 255, 255], (posX, posY), 10, width = 1)
+    pygame.draw.rect(surface, [255, 255, 255], (posX + 3, posY, 12, 1))
+    pygame.draw.rect(surface, [255, 255, 255], (posX - 15, posY, 12, 1))
+    pygame.draw.rect(surface, [255, 255, 255], (posX, posY - 15, 1, 12))
+    pygame.draw.rect(surface, [255, 255, 255], (posX, posY + 3, 1, 12))
